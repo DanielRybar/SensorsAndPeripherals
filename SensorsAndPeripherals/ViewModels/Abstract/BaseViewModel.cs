b15e94a0ce7948e6480052c10427de7910fcc7e0
Daniel Rybar <daniel.rybar1@tul.cz>
2025-10-29 12:50:50 +0100

    uprava sdk, workloadu, pridan abstraktni vm

---
+++ SensorsAndPeripherals/ViewModels/Abstract/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SensorsAndPeripherals.ViewModels.Abstract
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected bool SetProperty<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null!)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value)) return false;
+            backingField = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+
+        private string title = string.Empty;
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+
+        private bool isBusy = false;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set => SetProperty(ref isBusy, value);
+        }
+    }
+}
