01ccd2bf01ed53d6124ae0a3b75b53a6da6a6155
Daniel Rybar <daniel.rybar1@tul.cz>
2026-01-22 15:19:39 +0100

    vymena fontu, dokonceni vyberu senzoru

---
+++ SensorsAndPeripherals/Views/Sensors/GravitySensorPage.xaml.cs
@@ -0,0 +1,13 @@
+using SensorsAndPeripherals.Views.Abstract;
+
+namespace SensorsAndPeripherals.Views.Sensors;
+
+public partial class GravitySensorPage : ApplicationPage
+{
+	public GravitySensorPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override string InfoText => "";
+}
