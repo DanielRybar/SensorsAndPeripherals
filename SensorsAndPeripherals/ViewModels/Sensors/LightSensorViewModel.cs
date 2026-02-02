582748662e11137e3e4672a572b984b490702ce3
Daniel Rybar <daniel.rybar1@tul.cz>
2026-02-02 12:18:27 +0100

    zarovka

--- SensorsAndPeripherals/ViewModels/Sensors/LightSensorViewModel.cs
+++ SensorsAndPeripherals/ViewModels/Sensors/LightSensorViewModel.cs
@@ -6,20 +6,38 @@ namespace SensorsAndPeripherals.ViewModels.Sensors
 {
     public class LightSensorViewModel : SensorViewModel<ILightSensorService, LightSensorChangedEventArgs>
     {
+        #region variables
+        private readonly double maxLogLux = 5.0; // log10(100000) = 5
+        #endregion
+
         #region constructor
         public LightSensorViewModel()
         {
-            DisplayText = $"{0:F2} lx";
+            DisplayText = $"{0:F0} lx";
         }
         #endregion
 
         #region event handlers
         protected override void OnReadingChanged(object? sender, LightSensorChangedEventArgs e)
         {
+            float lux = e.Illuminance;
+            double safeLux = Math.Max(lux, 1);
+            double logVal = Math.Log10(safeLux);
+            double intensity = Math.Clamp(logVal / maxLogLux, 0f, 1f);
+
             MainThread.BeginInvokeOnMainThread(() =>
             {
-                DisplayText = $"{e.Illuminance:F2} lx";
+                DisplayText = $"{lux:F0} lx";
+                Intensity = intensity;
             });
+        }
+        #endregion
+
+        #region properties
+        public double Intensity
+        {
+            get;
+            set => SetProperty(ref field, value);
         }
         #endregion
     }
