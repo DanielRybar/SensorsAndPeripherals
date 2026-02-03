06d5be500d4725c4f6bdda4ad1c46837f64c21a2
Daniel Rybar <daniel.rybar1@tul.cz>
2026-02-03 14:08:05 +0100

    proximity dokonceni

---
+++ SensorsAndPeripherals/ViewModels/Sensors/ProximitySensorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SensorsAndPeripherals.Interfaces;
+using SensorsAndPeripherals.Models.CustomEventArgs;
+using SensorsAndPeripherals.ViewModels.Abstract;
+
+namespace SensorsAndPeripherals.ViewModels.Sensors
+{
+    public class ProximitySensorViewModel : SensorViewModel<IProximitySensorService, ProximitySensorChangedEventArgs>
+    {
+        #region constructor
+        public ProximitySensorViewModel()
+        {
+            DisplayText = $"{0:F2} cm";
+        }
+        #endregion
+
+        #region event handlers
+        protected override void OnReadingChanged(object? sender, ProximitySensorChangedEventArgs e)
+        {
+            float distance = e.Distance;
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                DisplayText = $"{distance:F2} cm";
+            });
+        }
+        #endregion
+
+        #region properties
+        protected override SensorSpeed DefaultSpeed => SensorSpeed.UI;
+        #endregion
+    }
+}
--- SensorsAndPeripherals/Views/Sensors/ProximitySensorPage.xaml.cs
+++ SensorsAndPeripherals/Views/Sensors/ProximitySensorPage.xaml.cs
@@ -1,13 +1,26 @@
+using SensorsAndPeripherals.ViewModels.Sensors;
 using SensorsAndPeripherals.Views.Abstract;
 
 namespace SensorsAndPeripherals.Views.Sensors;
 
 public partial class ProximitySensorPage : ApplicationPage
 {
-	public ProximitySensorPage()
-	{
-		InitializeComponent();
-	}
+    private readonly ProximitySensorViewModel viewModel;
+
+    public ProximitySensorPage()
+    {
+        InitializeComponent();
+        BindingContext = viewModel = new ProximitySensorViewModel();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        if (viewModel.IsMonitoring)
+        {
+            viewModel.StopSensor();
+        }
+    }
 
     protected override string InfoText => "";
 }
