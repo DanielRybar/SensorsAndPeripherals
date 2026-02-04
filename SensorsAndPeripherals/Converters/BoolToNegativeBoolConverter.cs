bce1458b4b40812017e25f96eb8c44cc155bde09
Daniel Rybar <daniel.rybar1@tul.cz>
2026-02-04 16:25:20 +0100

    hlasky, obrazky

---
+++ SensorsAndPeripherals/Converters/BoolToNegativeBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace SensorsAndPeripherals.Converters
+{
+    public class BoolToNegativeBoolConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool status)
+            {
+                return !status;
+            }
+            return value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
