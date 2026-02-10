d9babf386c7632088a6cbc34e5cc1c93aa3578ee
Daniel Rybar <daniel.rybar1@tul.cz>
2026-02-10 18:46:27 +0100

    vibrace, styly, ikonky

--- SensorsAndPeripherals/ViewModels/Peripherals/VibrationViewModel.cs
+++ SensorsAndPeripherals/ViewModels/Peripherals/VibrationViewModel.cs
@@ -1,5 +1,7 @@
-﻿using SensorsAndPeripherals.Interfaces.Peripherals;
+﻿using CommunityToolkit.Maui.Alerts;
+using SensorsAndPeripherals.Interfaces.Peripherals;
 using SensorsAndPeripherals.ViewModels.Abstract;
+using System.Windows.Input;
 
 namespace SensorsAndPeripherals.ViewModels.Peripherals
 {
@@ -8,15 +10,56 @@ namespace SensorsAndPeripherals.ViewModels.Peripherals
         #region constructor
         public VibrationViewModel()
         {
+            VibrateCommand = new Command(async () =>
+            {
+                if (Duration > MaxDuration || Duration < MinDuration)
+                {
+                    await Toast.Make($"Zadejte validní dobu vibrace mezi {MinDuration} a {MaxDuration} ms!").Show();
+                    return;
+                }
+                CancelVibration();
+                peripheralService.Vibrate(Duration);
+            },
+            () => IsSupported);
+            PerformShortHapticFeedbackCommand = new Command(() =>
+            {
+                PerformHapticFeedback(HapticFeedbackType.Click);
+            },
+            () => IsSupported);
+            PerformLongHapticFeedbackCommand = new Command(() =>
+            {
+                PerformHapticFeedback(HapticFeedbackType.LongPress);
+            },
+            () => IsSupported);
         }
+        #endregion
+
+        #region commands
+        public ICommand VibrateCommand { get; private set; }
+        public ICommand PerformShortHapticFeedbackCommand { get; private set; }
+        public ICommand PerformLongHapticFeedbackCommand { get; private set; }
         #endregion
 
         #region methods
         public void CancelVibration() => peripheralService.CancelVibration();
+
+        private void PerformHapticFeedback(HapticFeedbackType hfType)
+        {
+            CancelVibration();
+            peripheralService.PerformHapticFeedback(hfType);
+        }
         #endregion
 
         #region properties
+        public int Duration
+        {
+            get;
+            set => SetProperty(ref field, value);
+        }
 
+        public int MinDuration => 1;
+        public int MaxDuration => 10000;
+        public string DurationText => $"{MinDuration} až {MaxDuration} ms";
         #endregion
     }
 }
